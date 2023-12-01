34750007303d66db64e71145e2f430c454c30eb4
一輝 岩村 <kazuki.2004.10@outlook.jp>
2023-12-01 10:32:09 +0900

    修正

--- moji/moji.cpp
+++ moji/moji.cpp
@@ -1,8 +1,23 @@
-﻿#include "loadData.h"
-#include "number.h"
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 int main() {
-	int num = 0;
-	loadData("data.txt", &num);
-	number(num);
-	//修正
+    FILE* file;
+    char line[100];
+    file = fopen("data.txt", "r");
+    while (fgets(line, sizeof(line), file) != NULL) {
+        for (int i = 0; line[i] != '\0'; i++) {
+            if (line[i] == '1') {
+                printf("*");
+            }
+            else if (line[i] == '0') {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    fclose(file);
+
+    return 0;
 }
--- moji/number.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void number(int num);
