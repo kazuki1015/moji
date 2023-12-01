1bae779d4e296dda31fd26dfe7cae5cdc2f9e3d8
一輝 岩村 <kazuki.2004.10@outlook.jp>
2023-12-01 10:40:23 +0900

    外部変数をなくしました

--- moji/moji.cpp
+++ moji/moji.cpp
@@ -3,20 +3,27 @@
 
 int main() {
     FILE* file;
-    char line[100];
+
+   
     file = fopen("data.txt", "r");
-    while (fgets(line, sizeof(line), file) != NULL) {
-        for (int i = 0; line[i] != '\0'; i++) {
-            if (line[i] == '1') {
-                printf("*");
-            }
-            else if (line[i] == '0') {
-                printf(" ");
-            }
+
+    
+
+   
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '1') {
+            printf("*");
         }
-        printf("\n");
+        else if (c == '0') {
+            printf(" ");
+        }
+        else if (c == '\n') {
+            printf("\n");
+        }
     }
 
+    
     fclose(file);
 
     return 0;
